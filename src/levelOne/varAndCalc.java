bde56fd8133554158ca857d2615ac6224e0d23b5
IvanTushynski <tushinski.ivan@gmail.com>
2023-05-27 10:03:50 +0300

    Initial commit

---
+++ src/levelOne/varAndCalc.java
@@ -0,0 +1,24 @@
+package levelOne;
+
+public class varAndCalc {
+    public void main(String[] args) {
+
+        Integers intOne = new Integers();
+        Strings strOne = new Strings();
+        Calculation clacOne = new Calculation();
+    }
+
+    public class Integers{
+
+        int a,b,c,d;
+    }
+    public class Strings{
+
+        String g,h,j;
+
+    }
+
+    public class Calculation{
+
+    }
+}
